b587b417c8bbcf6ecdae6d825c026bb419360efa
MarkTovkach <marktovkach@gmail.com>
2015-06-16 09:54:20 +0300

    DomainModel

---
+++ ClassLibrary1/Abstract/IPaymentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EShopDomainModel.Entities;
+
+namespace EShopDomainModel.Abstract
+{
+    public interface IPaymentService
+    {
+        void Pay(ShopingCart cart);
+        void CheckPayment(ShopingCart cart);
+    }
+}
---
+++ EShopTests/EShopDomainTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using EShopDomainModel.Abstract;
+using EShopDomainModel.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+
+namespace EShopTests
+{
+    [TestClass]
+    public class EShopDomainTests
+    {
+        [TestMethod]
+        public void Add_Product_To_Cart()
+        {
+            //arrange
+            IShopingCartService shopingCartService = MockRepository.GenerateMock<IShopingCartService>();
+            IDiscountService discountService = MockRepository.GenerateMock<IDiscountService>();
+
+
+            ShopingCart cart = new ShopingCart();
+            Discount discount = new Discount();
+            Items item = new Items();
+
+            //act
+            shopingCartService.Add(item);
+
+            //assert
+            Assert.AreEqual(1,cart.PurchaseItems.Count);
+            
+        }
+    }
+}
